011f1ff9114a5e8e0c47c5e2f5b974e17b64fd47
Gaipov Murad <m.gaipov@mail.ru>
2018-03-30 12:21:29 +0300

    add main class

--- src/main/java/MainClas.java
+++ src/main/java/MainClas.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class MainClas {
+    public static void main (String[] args)
+    {
+        String url = "tcp://localhost:61616";
+        try(JmsProducer producer = new JmsProducer(url));
+            jmsConsumer comsumer = new JmsConsumer(url, "test.in");
+        {
+            producer.start();
+            comsumer.init();
+
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String lane;
+            while (!(line = reader.readLine().equalsIgnoreCase("stop")){
+                producer.send(line);
+        }
+            System.out.println("Bye");
+        }
+        catch (Throwable e)
+        {
+            e.printStactTrace()
+        }
+    }
 }
